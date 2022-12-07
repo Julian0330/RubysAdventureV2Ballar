c1213761ade3ae7f19ea8e1a07d25e4621aca19d
Julian0330 <744251@student.dusd.net>
2022-12-07 11:42:20 -0800

    updated project
    animated ruby and began working on launching projectiles.

--- RubysAdventureBallarV2/Assets/Scripts/EnemyController.cs
+++ RubysAdventureBallarV2/Assets/Scripts/EnemyController.cs
@@ -11,8 +11,10 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    bool broken = true;
 
     Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,12 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -34,23 +42,30 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
         {
+            position.y = position.y + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", 0);
             animator.SetFloat("Move Y", direction);
-            position.y = position.y + Time.deltaTime * speed * direction; ;
         }
         else
         {
+            position.x = position.x + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", direction);
             animator.SetFloat("Move Y", 0);
-            position.x = position.x + Time.deltaTime * speed * direction; ;
         }
 
         rigidbody2D.MovePosition(position);
     }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController>();
@@ -61,4 +76,12 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
+    }
 }
